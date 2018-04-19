adcf691075eb235d408ace25215a1071349a4167
Brandon Rush <BrandonRush@protonmail.com>
2018-04-19 06:42:32 -0400

    Security

--- src/server.js
+++ src/server.js
@@ -3,19 +3,19 @@ const app = express();
 const sslRedirect = require('heroku-ssl-redirect');
 const port = process.env.PORT || 3300;
 
-app.use((req, res, next) => {
-  res.setHeader('Strict-Transport-Security', 'max-age=63072000');
-  res.setHeader('Content-Security-Policy', "frame-ancestors 'none'");
-  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
-  res.setHeader('X-XSS-Protection', '1; mode=block');
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader(
-    'Content-Security-Policy-Report-Only',
-    "default-src *; img-src 'self'; object-src 'self' https://cdn.rawgit.com/; script-src 'self' https://cdn.rawgit.com/; style-src 'self' https://cdnjs.cloudflare.com/"
-  );
+// app.use((req, res, next) => {
+//   res.setHeader('Strict-Transport-Security', 'max-age=63072000');
+//   res.setHeader('Content-Security-Policy', "frame-ancestors 'none'");
+//   res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+//   res.setHeader('X-XSS-Protection', '1; mode=block');
+//   res.setHeader('X-Content-Type-Options', 'nosniff');
+//   res.setHeader(
+//     'Content-Security-Policy-Report-Only',
+//     "default-src *; img-src 'self'; object-src 'self' https://cdn.rawgit.com/; script-src 'self' https://cdn.rawgit.com/; style-src 'self' https://cdnjs.cloudflare.com/"
+//   );
 
-  next();
-});
+//   next();
+// });
 
 // enable ssl redirect
 app.use(sslRedirect());
