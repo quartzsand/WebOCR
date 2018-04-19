48305d72bd1ac35c3974f1a9736b43a616af0045
Brandon Rush <BrandonRush@protonmail.com>
2018-04-19 06:00:00 -0400

    Security

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 const sslRedirect = require('heroku-ssl-redirect');
+const port = process.env.PORT || 3300;
+
+app.use((req, res, next) => {
+  res.setHeader('Strict-Transport-Security', 'max-age=63072000');
+  res.setHeader('Content-Security-Policy', "frame-ancestors 'none'");
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('Content-Security-Policy', 'default-src https:');
+
+  next();
+});
 
 // enable ssl redirect
 app.use(sslRedirect());
 app.use(express.static('public'));
-
-const port = process.env.PORT || 3300;
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
